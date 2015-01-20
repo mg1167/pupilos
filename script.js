7142934e36d6db454e33f498e31838701cee6978
Moises Guerra <moiimkii@gmail.com>
2015-01-20 11:45:28 -0600

    Angular

---
+++ script.js
@@ -0,0 +1,9 @@
+function AlumnosController($scope){
+		$scope.alumnos=[
+			{nombre: "Moises Guerra", Genero: "Masculino", trabajo: "Cognits Team"},
+			{nombre: "Alejandro Batres", Genero: "Masculino", trabajo: "Cognits Team"},
+			{nombre: "Alejandro Tamayo", Genero: "Masculino", trabajo: "Cognits Team"},
+			{nombre: "Marcos Rodriguez", Genero: "Masculino", trabajo: "Cognits Team"},
+			{nombre: "Erick Gonzales", Genero: "Masculino", trabajo: "Cognits Team"}
+];
+};
