a5c85333dae299f1b0eccc5ca5f52eed8c715b11
Moises Guerra <moiimkii@gmail.com>
2015-01-22 08:41:31 -0600

    Version_f

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ function AlumnosController($scope){
 			{nombre: "Alejandro Tamayo", Genero: "Masculino", trabajo: "Cognits Team"},
 			{nombre: "Marcos Rodriguez", Genero: "Masculino", trabajo: "Cognits Team"},
 			{nombre: "Erick Gonzales", Genero: "Masculino", trabajo: "Cognits Team"}
-];
-};
+		];
+			//El scope siguiente capturara los valores de entrada.
+		$scope.Save = function(){
+			$scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre, Genero:$scope.nuevoAlumno.Genero, trabajo:$scope.nuevoAlumno.trabajo});
+			alert("Nuevo integrante de Cognits agregado correctamente.");
+			console.log("Registro añadido");
+		}
+}
+			//scope: es el alcance o limite al que se quiere llegar.
